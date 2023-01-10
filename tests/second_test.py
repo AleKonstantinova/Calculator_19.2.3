807fa9aaa515991cf31a13f7e11a982e7e9722f4
Alena <slastena405@gmail.com>
2023-01-10 19:58:22 +0600

    fix tests

--- tests/second_test.py
+++ tests/second_test.py
@@ -1,5 +1,5 @@
-import pytest
 from app.calculator import Calculator
+
 
 class TestCalc:
     def setup(self):
@@ -8,17 +8,10 @@ class TestCalc:
     def test_calculate_correctly(self):
         assert self.calc.division(self, 8, 2) == 4
 
-    def test_catculation_filed(self):
-        assert self.calc.division(self, 6, 0)
-
     def test_summa_correctly(self):
         assert self.calc.adding(self, 9, 4) == 13
-
-    def test_summa_filed(self):
-        assert self.calc.adding(self, 6, 4) == 9
 
     def test_subtract_correctly(self):
         assert self.calc.subtraction(self, -5, 6) == -11
 
-    def test_subtraction_filed(self):
-        assert self.calc.subtraction(self, 20, 10) == 5
+
